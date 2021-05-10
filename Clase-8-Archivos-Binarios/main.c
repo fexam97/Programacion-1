b42184bb813b8f8eb311cf4fd0af55f3ba2c29b6
Fernando Meza <70779182+fexam97@users.noreply.github.com>
2021-05-10 20:40:20 -0300

    Update main.c

--- Clase-8-Archivos-Binarios/main.c
+++ Clase-8-Archivos-Binarios/main.c
@@ -1,29 +1,25 @@
+/************** la unica forma de generar archivo es haciendo      FILE* prodF = fopen("Productos.dat", "rb") 
+y no con     FILE* prodF = fopen(nombre, "rb"); *******************************/
+
+
 #include "productos.h"
 
 /// Archivos.exe Productos.dat
-int main(int argc, char* argv[])        // argc: argumento cuenta
+
+int main(int argc, char* argv[])        // argc: argumento cuenta ( no lo usó)
                                         // argv[]: vector de argumento
 
 {
-    if(argc != 2)
+    if( generarArchivo(argv[ARG_NOM_ARCH]) == ERR_ARCHIVO)
     {
         puts("Error invocando al programa. Cantidad incorrecta de argumentos.");
-        return ERR_INV_ARG;
+        return ERR_ARCHIVO;
     }
 
-/*
-  if(generarArchivo(argv[ARG_NOM_ARCH]) == ERR_ARCHIVO)
-    {
-      puts("No se pudo abrir el archivo.");
-      return ERR_ARCHIVO;
-    }
-*/
-
-    printf("puto");
 
     producto prod;
 
-    FILE* prodF = fopen(argv[ARG_NOM_ARCH], "rb");
+    FILE* prodF = fopen("Productos.dat", "rb");
 
     if(!prodF)
     {
@@ -40,6 +36,8 @@ int main(int argc, char* argv[])        // argc: argumento cuenta
     }
 
     fclose(prodF);
+
+
 
     return 0;
 }
