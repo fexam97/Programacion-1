c0222d03f3b06c24eee7e25ce07d90a3f2b5af10
Fernando Meza <70779182+fexam97@users.noreply.github.com>
2021-05-10 23:26:38 -0300

    Update main.c

--- Clase-8-Archivos-Binarios/main.c
+++ Clase-8-Archivos-Binarios/main.c
@@ -5,23 +5,56 @@ y no con     FILE* prodF = fopen(nombre, "rb"); *******************************/
 #include "productos.h"
 
 /// Archivos.exe Productos.dat
-int main(int argc, char* argv[])        // argc: argumento cuenta
-                                        // argv[]: vector de argumento
+int main(int argc, char* argv[])        // argc: contador de argumentos, algo asi entendi
+                                        // argv[]: vector de argumentos
 
 {
-    if( generarArchivo(argv[ARG_NOM_ARCH]) == ERR_ARCHIVO)
+    //if( generarArchivo(argv[ARG_NOM_ARCH]) == ERR_ARCHIVO)
+
+    if(argc != 3) // project -> set programs' arguments -> escriben Productos.dat 10 -> OK y listo deberia leer bien
     {
         puts("Error invocando al programa. Cantidad incorrecta de argumentos.");
-        return ERR_ARCHIVO;
+        return ERR_INV_ARG;
     }
 
+    puts("Antes de actualizar:");
 
-    if(LeerArchivo(argv[ARG_NOM_ARCH]) == ERR_ARCHIVO) // argv[1] == 1
+    int ret = LeerArchivo(argv[ARG_NOM_ARCH]);
+
+    if(ret == ERR_ARCHIVO)
     {
         puts("No se pudo leer el archivo");
         return ERR_ARCHIVO;
     }
 
+  /********************* ACTUALIZO ********************************/
+  
+    float porcentaje = atof(argv[ARG_PORC_ACT]); // atof covierte cadena en float
+
+    ret = actualizarPrecioProductos(argv[ARG_NOM_ARCH], porcentaje);
+
+    if(ret != TODO_OK)
+    {
+        puts("No se pudo actualizar el archivo");
+        return ret;
+    }
+
+    /************ VUELVO A MOSTRAR EL ARCHIVO ***************/
+
+    puts("\n Despues de actualizar:");
+
+     ret = LeerArchivo(argv[ARG_NOM_ARCH]);
+
+    if(ret == ERR_ARCHIVO)
+    {
+        puts("No se pudo leer el archivo");
+        return ERR_ARCHIVO;
+    }
+
+
+    puts("\n\n fijenseN como cambio el precio");
+
+    // si compilan muchas veces, veran que los precios estan aumentando
 
     return 0;
 }
